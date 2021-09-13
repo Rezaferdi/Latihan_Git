982c3c1722ee1c47e25603d72b3db5e0f87c722f
Rezaferdi <90586624+Rezaferdi@users.noreply.github.com>
2021-09-13 16:53:28 +0700

    Add files via upload

---
+++ latihan1/latihan1.php
@@ -0,0 +1,9 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "perkenalkan saya reza ferdiansyah<br>";
+    echo "Saya mahasiswa kelas 19.3A.05";
+    }
+}
